e7380c62e25b3246a9df62c21ca38304e874d689
LuanMetz <euluanmetz@gmail.com>
2025-09-01 20:17:57 -0300

    Add files via upload

---
+++ # array.py
@@ -0,0 +1,13 @@
+# Array.py
+colegas = ["João", "Amanda", "Felipe", "Ana", "Roberto"]
+
+# Elementos da Array e seus elementos
+print("Primeiro elemento:", colegas[0])     # Índice 0
+print("Terceiro elemento:", colegas[2])     # Índice 2
+print("Último elemento:", colegas[4])       # Índice 4
+
+# Verificando se "Carla" está na lista
+if "Carla" in colegas:
+    print("Carla está na lista!")
+else:
+    print("Carla não foi encontrada na lista.")
